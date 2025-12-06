798c5fec65f3a52ec398898cfd1848dad115bf5e
Chiamaka Uyanna <chiamakauyanna@gmail.com>
2025-12-06 13:45:14 +0100

    upgraded Next.js

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -3,10 +3,17 @@ import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { Provider } from "react-redux";
 
+import { Nunito, Fredoka } from "next/font/google";
+
+const nunito = Nunito({ subsets: ["latin"], variable: "--font-nunito" });
+const fredoka = Fredoka({ subsets: ["latin"], variable: "--font-fredoka" });
+
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <Provider store={store}>
-      <Component {...pageProps} />
+      <main className={`${nunito.variable} ${fredoka.variable}`}>
+        <Component {...pageProps} />
+      </main>
     </Provider>
   );
 }
